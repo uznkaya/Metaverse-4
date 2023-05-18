aab482c2cd6c19f3694fbe891b1a29c90d749e49
Mehmet Can Uzunkaya <mehmetcan.uzunkaya@icloud.com>
2023-05-18 17:48:18 +0300

    May 18

--- My project/Assets/LoadData.cs
+++ My project/Assets/LoadData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadData : MonoBehaviour
 {
@@ -18,5 +19,13 @@ public class LoadData : MonoBehaviour
         {
             loadText.text = "There is no registered player";
         }
+    }
+    public void Delete()
+    {
+        PlayerPrefs.DeleteKey("Name");
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
     }
 }
